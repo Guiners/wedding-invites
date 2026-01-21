0b678d1819739407a70da5cf3bb4af035b9acdae
Guiners <rkoza@softserveinc.com>
2026-01-21 15:43:34 +0100

    adding fonts, karo save the date

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
-from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def read_root():
-    return FileResponse("templates/invitation-base-1.html")
+def index(request: Request):
+    return templates.TemplateResponse("invitation_base1.html", {"request": request})
